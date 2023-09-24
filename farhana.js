5956341c2879a3da66d5226c6ae0e69b5c7e10ee
farhanaa-noor <80719003+farhanaa-noor@users.noreply.github.com>
2023-09-24 18:46:32 -0400

    Update farhana.js with callfunctions

--- farhana.js
+++ farhana.js
@@ -1,15 +1,45 @@
+/* myEach */
 function myEach(arr, callback){
     for(let i=0; i< arr.length;i++){
-        callback(arr[i]);
+        callback(arr[i],i);
     }
 }
+
+function runMyEach(){
+    let inputArrayText = document.getElementById('inputArray').value;
+    let newArr = [];
+    newArr = JSON.parse(inputArrayText);
+    myEach(newArr, resultMyEach);
+}
+
+function resultMyEach(value, index){
+    document.getElementById('resultArray').textContent += "Value in index "+index+" is "+value+"\n";
+}
+
+
+/* myMap */
 
 function myMap(arr, callback){
     let newArr = [];
     for(let i=0; i<arr.length;i++){
         newArr[i] = callback(arr[i]);
     }
+    document.getElementById('resultArray2').textContent = "Array "+"["+newArr+"]";
 }
+
+function runMyMap(){
+    let inputArrayText = document.getElementById('inputArray2').value;
+    let newArr = [];
+    newArr = JSON.parse(inputArrayText);
+    myMap(newArr, resultMyMap);
+}
+
+function resultMyMap(value){
+    return value*2;
+}
+
+
+/* myFilter */
 
 function myFilter(arr, callback){
     let newArr = [];
@@ -20,5 +50,22 @@ function myFilter(arr, callback){
             newArr[j] = arr[i];
             j++;
         }
+    }
+    document.getElementById('resultArray3').textContent = "Array "+"["+newArr+"]";
+}
+
+function runMyFilter(){
+    let inputArrayText = document.getElementById('inputArray3').value;
+    let newArr = [];
+    newArr = JSON.parse(inputArrayText);
+    myFilter(newArr, resultMyFilter);
+}
+
+function resultMyFilter(value){
+    if(value < 10){
+        return 1;
+    }
+    else{
+        return 0;
     }
 }
