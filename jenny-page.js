0af90a604b37fe98dc2a443352c7b9ebda936054
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-25 12:12:13 -0400

    Added implementation to display array and create one

--- jenny-page.js
+++ jenny-page.js
@@ -1,3 +1,88 @@
+
+// input div
+let inputIncludesArray = document.body.querySelector("#includes-array");
+let inputIncludesArrayDisplay = document.body.querySelector("#includes-array-display");
+let inputIncludesArrayCheck = document.body.querySelector("#includes-array-check");
+
+// argument div
+let inputIncludesTarget = document.body.querySelector("#includes-target");
+
+// result div
+let inputIncludesResultText = document.body.querySelector("#includes-result-text");
+
+// button
+let inputIncludesButton = document.body.querySelector("#includes-button");
+
+let inputIncludesContent = null;
+let inputIncludesTargetElement = null;
+
+inputIncludesArray.addEventListener("input", () => {
+    inputIncludesContent = inputIncludesArray.value;
+});
+
+inputIncludesArrayCheck.addEventListener("click", () => {
+    inputIncludesArrayDisplay.value = displayArray();
+});
+
+inputIncludesTarget.addEventListener("input", () => {
+    inputIncludesTargetElement = inputIncludesTarget.value;
+});
+
+inputIncludesButton.addEventListener("click", () => {
+    if (inputIncludesContent !== null && inputIncludesTargetElement !== null) {
+        inputIncludesResultText.value = myIncludes(createArray(), inputIncludesTargetElement); 
+    }
+});
+
+// displays the possible result of individual elements provided by the user as a string
+function displayArray() {
+    let displayResult = "Is this what you expected:[";
+    let line = "";
+    if (inputIncludesContent !== null) {
+        for (let i=0; i<inputIncludesContent.length; i++) {
+            if (inputIncludesContent[i] !== " ") {
+                line += inputIncludesContent[i];
+            } else {
+                line += ", ";
+            }
+            if (i === inputIncludesContent.length - 1 && inputIncludesContent[i] !== " ") { // for last word
+                line += "]";
+            }
+        }
+    }
+
+    displayResult += line;
+
+    //console.log(displayResult);
+    return displayResult;
+}
+
+// creates array from individual elements provided by the user
+function createArray() {
+    let createdIncludesArray = [];
+    let word = "";
+    if (inputIncludesContent != null) {
+        for (let i=0; i<inputIncludesContent.length; i++) {
+            if (inputIncludesContent[i] !== " ") {
+                word += inputIncludesContent[i];
+            } else {
+                createdIncludesArray.push(word);
+                word = "";
+            }
+            if (i === inputIncludesContent.length - 1 && inputIncludesContent[i] !== " ") { // for last word
+                createdIncludesArray.push(word);
+                word = "";
+            }
+        }
+    }
+
+    //console.log(createdIncludesArray);
+    return createdIncludesArray;
+}
+
+
+// THE THREE FUNCTIONS BELOW 
+
 // takes in array and target, iterates through array
 // Example:
 // const numArr = [1,2,3,4,5];
@@ -31,8 +116,8 @@ function myIndexOf(arr, target) {
 
 // takes in arr and element to append
 // using length of arr, attaches to end of current arr
-//const numArr = [1, 2, 3, 4, 5];
-// myIndexOf(numArr, 7); // result is [1, 2, 3, 4, 5, 7]
+// const numArr = [1, 2, 3, 4, 5];
+// myIndexOf(numArr, 7); // Result: [1, 2, 3, 4, 5, 7]
 
 function myPush(arr, elementToAdd) {
     arr[arr.length] = elementToAdd;
