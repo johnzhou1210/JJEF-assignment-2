26ef5750fb70aeb1c830b266d01a77abfb1d5e12
John Zhou <chowjohn10@gmail.com>
2023-09-24 22:44:46 -0400

    moved implementations into isolated functions for modularity
    Also improved sample input for more realistic and practical use examples.

--- last-3-methods/js/grab-values.js
+++ last-3-methods/js/grab-values.js
@@ -1,18 +1,27 @@
 const grabValuesButton = document.querySelector("#invoke-grab-values-function");
 
+function grabValues() {
+    let arr = document.querySelector("#grab-values-arg-1").value.trim().split(",");
+    let result = [];
+    for (const i in arr) {
+      let currPair = arr[i];
+      let key = currPair.substring(0, currPair.indexOf(":"));
+      let val = currPair.substring(currPair.indexOf(":") + 1, currPair.length);
+      result[i] = val; // alternatively you could use result.push(), but I am unsure if that is allowed for this assignment even if I am not the one doing that recreation
+    }
+    return result;
+}
+
+function prettyFormat(arr) {
+    let result = "[";
+    for (const i in arr) {
+        result += arr[i] + (i != arr.length - 1 ? ", " : "]");
+    }
+    return result;
+}
+
 /* Button click event */
 grabValuesButton.addEventListener("click", () => {
-    let arr = document.querySelector("#grab-values-arg-1").value.trim().split(",");
-    let result = "[";
-
-    for (const i in arr) {
-        let currPair = arr[i]; 
-        let key = currPair.substring(0, currPair.indexOf(":"));
-        let val = currPair.substring(currPair.indexOf(":") + 1, currPair.length);
-        result += val + (i != arr.length - 1 ? ", " : "");
-    }
-
-    result += "]";
     let output = document.querySelector("#grab-values-result");
-    output.textContent = result;
+    output.textContent = prettyFormat(grabValues());
 });
