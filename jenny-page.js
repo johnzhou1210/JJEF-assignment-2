be1611f04c42aefef963d0d17327ae6eb6e08f08
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2023-09-24 21:27:53 -0400

    myIndexOf is created

--- jenny-page.js
+++ jenny-page.js
@@ -20,3 +20,28 @@ function myIncludes(arr, target) {
     }
     return false;
 }
+
+
+//     myIndexOf() Without using the native “Array.prototype.indexOf” method of JavaScript, 
+//     compose a function titled “myIndexOf” that will take in an array of elements 
+//     and returns the index of the first encounter of a target element (if it is found) 
+//     or -1 if that element does not exist within the input array.
+
+// takes in array and target
+// iterates through array using index i. 
+// Compare to target, if it is there then return the index else -1
+// Example:
+// const numArr = [1,2,3,4,5];
+// myIndexOf(numArr, 3); // 2
+// myIndexOf(numArr, 7); // -1
+
+function myIndexOf(arr, target) {
+    for (let i=0; i<arr.length; i++) {
+        if (arr[i] === target) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+//const numArr = [1, 2, 3, 4];
